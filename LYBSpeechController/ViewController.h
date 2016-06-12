d0f27323d167ccfed30e1a9884b1e0559a34190c
@LiYanBing <@gsliyanbing@163.com>
2016-06-12 16:12:23 +0800

    Initial Commit

---
+++ LYBSpeechController/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LYBSpeechController
+//
+//  Created by 李彦兵 on 16/6/12.
+//  Copyright © 2016年 Leo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ LYBSpeechController/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  LYBSpeechController
+//
+//  Created by 李彦兵 on 16/6/12.
+//  Copyright © 2016年 Leo. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
